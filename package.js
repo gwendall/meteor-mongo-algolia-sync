d004a53279ce4d4d95fa76bfc538ac87d8f7582b
Gwen <gwendall.esnault@gmail.com>
2015-10-14 18:13:32 +0200

    misc

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'gwendall:mongo-algolia-sync',
   summary: 'Automatically sync a Mongo collection to an Algolia index',
-  git: 'https://github.com/gwendall/meteor-mongo-algolia-sync.git'
+  git: 'https://github.com/gwendall/meteor-mongo-algolia-sync.git',
   version: '0.1.0'
 });
 
 Package.onUse(function(api, where) {
 
   api.use([
-    'mongo',
-    'underscore',
-    'lai:collection-extensions',
-    'matb33:collection-hooks',
+    'mongo@1.1.2',
+    'underscore@1.0.4',
+    'lai:collection-extensions@0.0.9',
+    'matb33:collection-hooks@0.8.1',
   ], 'server');
 
   api.imply([
-    'acemtp:algolia'
+    'acemtp:algolia@3.7.8'
   ], 'server');
 
   api.addFiles([
