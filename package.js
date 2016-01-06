dc36cabcf3a3ccf8cf0e1570d4eead34cafdf7dd
Patrick Lewis <Patrickml.pml@gmail.com>
2016-01-05 19:32:04 -0500

    Fixed #2 Deprecation + Updated Packages

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'gwendall:mongo-algolia-sync',
   summary: 'Automatically sync a Mongo collection to an Algolia index',
   git: 'https://github.com/gwendall/meteor-mongo-algolia-sync.git',
-  version: '0.1.1'
+  version: '0.1.2'
 });
 
 Package.onUse(function(api, where) {
@@ -10,12 +10,12 @@ Package.onUse(function(api, where) {
   api.use([
     'mongo@1.1.2',
     'underscore@1.0.4',
-    'lai:collection-extensions@0.0.9',
-    'matb33:collection-hooks@0.8.1',
+    'lai:collection-extensions@0.2.1_1',
+    'matb33:collection-hooks@0.7.13',
   ], 'server');
 
   api.imply([
-    'acemtp:algolia@3.7.8'
+    'acemtp:algolia@3.9.1'
   ], 'server');
 
   api.addFiles([
